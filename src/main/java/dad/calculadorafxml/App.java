6e8d01e78e68e18c63e34cb37224cf68e697cbcc
Adrian Jiménez Mendoza <71876625+Adrian1998-java@users.noreply.github.com>
2022-01-11 13:32:25 +0000

    Aplicado el estilo Moderno y añadido el icono

--- src/main/java/dad/calculadorafxml/App.java
+++ src/main/java/dad/calculadorafxml/App.java
@@ -2,6 +2,7 @@ package dad.calculadorafxml;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import dad.calculadorafxml.controller.*;
@@ -17,6 +18,7 @@ public class App extends Application{
 		Scene scene = new Scene(controller.getView(), 400, 320);
 		
 		primaryStage.setTitle("Calculadora FXML");
+		primaryStage.getIcons().add(new Image("/image/calculator-32x32.png"));
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
